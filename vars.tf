12769498657ba054e74a9acbf213496e7ba07678
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-16 07:08:10 +0530

    Update Backend Steps.

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "vpc_cidr_block" {}
+variable "public_subnets" {}
+variable "web_subnets" {}
+variable "app_subnets" {}
+variable "db_subnets" {}
+variable "azs" {}
+variable "tags" {}
+variable "env" {}
+variable "default_vpc_cidr" {}
+
